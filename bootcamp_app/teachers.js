89af7954fc56b36eaf3adaa34a4975a9f9bd4e36
Rdarke <rickydarke1@gmail.com>
2021-09-15 19:25:48 -0700

    Adjust for injection hacks

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,15 +7,20 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
+const cohortsName = process.argv[2] || 'JUL02';
+const inputValues = [cohortsName]
+
+const queryString = `
 SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
 FROM teachers
 JOIN assistance_requests ON teachers.id = teacher_id
 JOIN students ON students.id = student_id
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+WHERE cohorts.name = $1
 ORDER BY teacher;
-`)
+`
+
+pool.query(queryString, inputValues)
 .then(res => {
   res.rows.forEach(row => {
     console.log(`This is the user data: ${row.cohort}: ${row.teacher}`);
